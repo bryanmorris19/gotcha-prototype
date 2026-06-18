5d0f23ad8518bf53b4057bc0aeb3af4f99f9ddd1
bryanmorris19 <7656699+bryanmorris19@users.noreply.github.com>
2026-06-17 22:49:15 -0600

    Convert game stage to 1080x1920

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const CACHE_NAME = "gotcha-mvp-v57";
+const CACHE_NAME = "gotcha-mvp-v58";
 const APP_SHELL = [
   "./",
   "./index.html",
@@ -9,9 +9,9 @@ const APP_SHELL = [
   "./admin.css?v=4",
   "./admin.js?v=4",
   "./admin.webmanifest",
-  "./styles.css?v=45",
+  "./styles.css?v=46",
   "./supabase-config.js?v=1",
-  "./app.js?v=44",
+  "./app.js?v=45",
   "./hunts.json",
   "./manifest.webmanifest",
   "./images/background.png",
